ce707ec599412591b9002584e05bb8921d6b0b0c
Daniel Nunes <daniel.f.nunes@ua.pt>
2025-10-26 21:52:41 +0000

    Deploy website - based on 63c231d6287b03bad583502c0453e901b74ca840

---
+++ assets/js/acecf23e.460ccf43.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkipss_docs=globalThis.webpackChunkipss_docs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/platform-docs/blog","blogTitle":"Blog","authorsListPath":"/platform-docs/blog/authors"}')}}]);
---
+++ assets/js/ba259a6f.59d862c6.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkipss_docs=globalThis.webpackChunkipss_docs||[]).push([[5453],{1152:s=>{s.exports=JSON.parse('{"metadata":{"permalink":"/platform-docs/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
